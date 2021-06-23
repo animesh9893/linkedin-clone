1e8d5c38e3d8c85f4abd00ea57aabaf7591b0803
Animesh Shrivatri <animesh.shrivatri2002@gmail.com>
2021-06-24 00:04:18 +0530

    card added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+
 import './App.css';
+import Home from './Component/Home';
+import NavBar from './Component/NavBar';
+
+const HomeMain = ()=>{
+  return(
+    <div className="home-fixed"><Home /></div>
+  )
+}
+
+const PageNotFound = ()=>{
+  return(
+    <div className="home-fixed"><h1>Page Not Found</h1></div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      Linkedin clone
+    <div className="main-full">
+      <div className="nav-fixed"><NavBar /></div>
+      <Switch>
+        <Route path="/" component={HomeMain} exact />
+        <Route component={PageNotFound} />
+      </Switch>
     </div>
   );
 }
